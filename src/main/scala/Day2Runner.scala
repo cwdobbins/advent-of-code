436adbe82d5dea0a2cf9415a6d6048a2906e12dc
Christopher Dobbins <christopher.dobbins@culebrasoftware.com>
2017-12-06 17:51:29 -0500

    finished day 2/part 2

--- src/main/scala/Day2Runner.scala
+++ src/main/scala/Day2Runner.scala
@@ -5,8 +5,16 @@ import scala.io.Source
 object Day2Runner {
 
   def main(args: Array[String]) = {
+    assert(minMaxDiffLine("1 3 5 7") == 6)
+    assert(divisibleResultLine("5 9 2 8") == 4)
+
+    //Part 1
     println((for (line <- Source.fromFile("src/main/resources/input/day2").getLines())
       yield minMaxDiffLine(line)).reduce {_ + _})
+
+    //Part 2
+    println((for (line <- Source.fromFile("src/main/resources/input/day2").getLines())
+          yield divisibleResultLine(line)).reduce {_ + _})
   }
 
   def minMaxDiffLine(in: String): Int = {
@@ -19,5 +27,14 @@ object Day2Runner {
       if (next > max) max = next
     }
     Math.abs(max - min)
+  }
+
+  def divisibleResultLine(in: String): Int = {
+    val map = in.split("\\s+").map {c => Integer.parseInt(c.toString(), 10)}.par
+    val things = map.map(c => map.filter(d => c != d && c % d == 0).map(d => (c, d)))
+                      .filter {_.nonEmpty}(0)(0)
+    if (things._1 > things._2)
+      things._1 / things._2
+    else things._2 / things._1
   }
 }

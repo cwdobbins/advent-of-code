39b5b6e4f5a2a8513ab178cda3687891866e8e1c
Christopher Dobbins <christopher.dobbins@culebrasoftware.com>
2017-12-06 17:03:11 -0500

    completed day 1/part 2

--- src/main/scala/Day1Runner.scala
+++ src/main/scala/Day1Runner.scala
@@ -3,8 +3,10 @@ import scala.io.Source
 object Day1Runner {
 
   def main(args: Array[String]) = {
+    assert(processCodePart2("123123") == 12)
+
     for (line <- Source.fromFile("src/main/resources/input/day1").getLines()) {
-      println(processCode(line))
+      println(processCodePart2(line))
     }
   }
 
@@ -13,13 +15,24 @@ object Day1Runner {
     val last: Char = xlast
   }
 
-  def processCode(in: String): String = {
+  def processCode(in: String): Int = {
     in.foldRight(new AccHelper(0, 'x'))((nextChar: Char, acc: AccHelper) => {
       if (nextChar == acc.last) {
         new AccHelper(acc.acc + Integer.parseInt(acc.last.toString(), 10), nextChar)
       } else {
         new AccHelper(acc.acc, nextChar)
       }
-    }).acc.toString()
+    }).acc
+  }
+
+  def processCodePart2(in: String): Int  = {
+    in.zip(in.substring(in.length / 2).concat(in.substring(0, in.length / 2)))
+      .foldRight(0)((nextPair: (Char, Char), acc: Int) => {
+        if (nextPair._1 == nextPair._2) {
+          acc + Integer.parseInt(nextPair._1.toString(), 10)
+        } else {
+          acc
+        }
+      })
   }
 }

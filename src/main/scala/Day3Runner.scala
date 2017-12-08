7e58c115736dfae5ec7fd05a13844a367adedb88
Christopher Dobbins <christopher.dobbins@culebrasoftware.com>
2017-12-07 21:10:10 -0500

    Day 3 / Part 2

--- src/main/scala/Day3Runner.scala
+++ src/main/scala/Day3Runner.scala
@@ -3,10 +3,15 @@ import scala.annotation.tailrec
 object Day3Runner {
 
   def main(args: Array[String]) = {
-//    assert(calculateManhattanDist(79) == 6)
-//    assert(calculateManhattanDist(82) == 9)
-//    assert(calculateManhattanDist(1024) == 31)
+    assert(calculateManhattanDist(79) == 6)
+    assert(calculateManhattanDist(82) == 9)
+    assert(calculateManhattanDist(1024) == 31)
+
+    //Part 1
     println(calculateManhattanDist(277678))
+
+    //Part 2
+    println(computeValueAfter(277678))
   }
 
   def calculateManhattanDist(value: Int): Int = {
@@ -38,10 +43,62 @@ object Day3Runner {
     )
   }
 
-//  def calculateNextLargestDigit(value: Int): Int = {
-//     def recursiveFindNeighborCount(index: Int): Unit = {
-//
-//     }
-//    return 0
-//  }
+  type Pos = (Int, Int)
+
+  sealed trait Direction {
+    def x: Int
+    def y: Int
+  }
+
+  case object UPRIGHT extends Direction {val x = 1; val y = -1}
+  case object RIGHT extends Direction {val x = 1; val y = 0}
+  case object UPLEFT extends Direction {val x = -1; val y = -1}
+  case object LEFT extends Direction {val x = -1; val y = 0}
+  case object DOWNRIGHT extends Direction {val x = 1; val y = 1}
+  case object UP extends Direction {val x = 0; val y = -1}
+  case object DOWNLEFT extends Direction {val x = -1; val y = 1}
+  case object DOWN extends Direction {val x = 0; val y = 1}
+
+  type Cursor = (Pos, Direction, Set[Pos])
+
+  def add(pos1: Pos, direction: Direction): Pos = (pos1._1 + direction.x, pos1._2 + direction.y)
+
+  def next(cursor: Cursor): Cursor = {
+    val (position, direction, set) = cursor
+    val turn = direction match {
+      case DOWN => RIGHT
+      case UP => LEFT
+      case LEFT => DOWN
+      case RIGHT => UP
+    }
+
+    val nextTurn = add(position, turn)
+    val nextStraight = add(position, direction)
+    if (set.contains(nextTurn))
+      (nextStraight, direction, set + nextStraight)
+    else
+      (nextTurn, turn, set + nextTurn)
+  }
+
+  def computeValueAfter(in: Int) = {
+    val initial = ((0, 0), UP, Set((0, 0)))
+
+    lazy val VALUES: Stream[(Cursor, Map[Pos, Int])] =
+      Stream.cons((initial, Map((0, 0) -> 1)), VALUES.map { e =>
+        val (cursor, map) = e
+        val nextValue = next(cursor)
+        (nextValue, map + (nextValue._1 ->
+          (for {
+            dir <- List(UP, RIGHT, LEFT, DOWN, UPRIGHT, UPLEFT, DOWNRIGHT, DOWNLEFT)
+          } yield map.getOrElse(add(nextValue._1, dir), 0)).sum
+        ))
+      })
+
+    VALUES.find {
+      case ((position, _, _), map) => map.getOrElse(position, 0) >= in
+    } match {
+      case Some(e) => e._2(e._1._1).toString
+    }
+  }
 }
+
